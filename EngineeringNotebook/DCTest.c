15a457fff011fe0cfee9cca26706f564532180d9
drohrbaugh9 <drohrbaugh9@users.noreply.github.com>
2014-12-16 16:55:21 -0700

    Format DCTest.c

--- EngineeringNotebook/DCTest.c
+++ EngineeringNotebook/DCTest.c
@@ -1,18 +1,13 @@
 #include "JoystickDriver.c"
 
-task main()
-{
-	while(true)
-	{
+task main() {
+	while(true) {
 		//each stick controls a motor
 		getJoystickSettings(joystick);
 
-		if(joystick.joy1_y1 < 20 && joystick.joy1_y1>-20)
-		{
+		if(joystick.joy1_y1 < 20 && joystick.joy1_y1>-20) {
 			motor[dc1] = 0;
-		}
-		else
-		{
+		} else {
 			motor[dc1] = joystick.joy1_y1;
 		}
 	}
