60903b48f2608cd0d9fd3fafa6dce0da7ecf53cb
drohrbaugh9 <drohrbaugh9@users.noreply.github.com>
2014-12-16 16:57:01 -0700

    Format ServoTest.c

--- EngineeringNotebook/ServoTest.c
+++ EngineeringNotebook/ServoTest.c
@@ -1,15 +1,13 @@
 #include "JoystickDriver.c"
 
-task main()
-{
-	while(true)
-	{
+task main() {
+	while(true) {
 		getJoystickSettings(joystick);
 		
-		if(joy1Btn(1)){
+		if(joy1Btn(1)) {
 			servo[servo1] = 255;
 		}
-		if(joy1Btn(2)){
+		if(joy1Btn(2)) {
 			servo[servo1] = 0;
 		}
 	}
